63babd97829752b78e962d8932d6215c1b8ab2f1
almaprintes <almaprint.es@gmail.com>
2026-08-07 11:39:07 +0100

    Activate reference-derived terrain materials

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuScene.js';
-import { RaceScene } from './scenes/RaceSurfaceMaterialBlendScene.js';
+import { RaceScene } from './scenes/RaceMaterialScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeShopScene.js';
 import { GarageScene } from './scenes/GarageScene.js';
 import { SettingsScene } from './scenes/SettingsScene.js';
