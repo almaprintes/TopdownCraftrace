7ac45f1eb39866ede50a00e016cfe94d1d3cc70a
almaprintes <almaprint.es@gmail.com>
2026-08-07 13:54:05 +0100

    Activate longitudinal asphalt mapping pass

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuScene.js';
-import { RaceScene } from './scenes/RaceMaterialScene.js';
+import { RaceScene } from './scenes/RaceSurfaceLongitudinalScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeShopScene.js';
 import { GarageScene } from './scenes/GarageScene.js';
 import { SettingsScene } from './scenes/SettingsScene.js';
