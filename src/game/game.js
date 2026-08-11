fe57c35ed723044f16a923615c9d54d93d2f3fc8
almaprintes <almaprint.es@gmail.com>
2026-08-11 09:28:37 +0100

    Use premium workshop and Retina rendering

--- src/game/game.js
+++ src/game/game.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuScene.js';
 import { RaceScene } from './scenes/RaceWideCameraPreviewScene.js';
-import { UpgradeShopScene } from './scenes/UpgradeWorkshopArcadeV3Scene.js';
+import { UpgradeShopScene } from './scenes/UpgradeWorkshopPremiumV2Scene.js';
 import { GarageScene } from './scenes/GarageScene.js';
 import { SettingsScene } from './scenes/SettingsScene.js';
 import { GarageDetailScene } from './scenes/GarageDetailScene.js';
@@ -16,10 +16,11 @@ export function createGame(parentId = 'app') {
     type: Phaser.AUTO,
     parent: parentId,
     backgroundColor: '#0b1020',
+    resolution: Math.min(window.devicePixelRatio || 1, 2),
     scene: [BootScene, MenuScene, GarageScene, SettingsScene, GarageDetailScene, RaceScene, AdminHubScene, UpgradeShopScene, CarEditorScene, TrackGarageScene, TrackStudioScene, TrackEditorScene],
     dom: { createContainer: true },
     scale: { mode: Phaser.Scale.RESIZE, autoCenter: Phaser.Scale.CENTER_BOTH },
     physics: { default: 'arcade', arcade: { debug: false } },
-    render: { pixelArt: false, antialias: true }
+    render: { pixelArt: false, antialias: true, antialiasGL: true, roundPixels: false }
   });
 }
