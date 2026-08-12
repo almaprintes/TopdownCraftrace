7be5f8549f758e4a1880850f7e479a7c6f9c3879
almaprintes <almaprint.es@gmail.com>
2026-08-12 16:35:03 +0100

    Activa celebración premium de récords

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuScene.js';
-import { RaceScene } from './scenes/RaceWideCameraPreviewScene.js';
+import { RaceScene } from './scenes/RaceTimingCelebrationScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopCraftAssetsScene.js';
 import { GarageScene } from './scenes/GarageScene.js';
 import { SettingsScene } from './scenes/SettingsScene.js';
