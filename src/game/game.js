602aa665cb2bc769803da0905de4f00c2d1bd084
almaprintes <almaprint.es@gmail.com>
2026-08-12 17:37:52 +0100

    Revert bicycle steering preview and restore original steering feel

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuScene.js';
-import { RaceScene } from './scenes/RaceBicycleHandlingScene.js';
+import { RaceScene } from './scenes/RaceTimingCelebrationVisibleScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopCraftAssetsScene.js';
 import { GarageScene } from './scenes/GarageScene.js';
 import { SettingsScene } from './scenes/SettingsScene.js';
