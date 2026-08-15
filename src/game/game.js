bf82757741d3a0415a72305351d0f4707ba800bc
almaprintes <almaprint.es@gmail.com>
2026-08-15 22:40:44 +0100

    Use enhanced main menu track presentation

--- src/game/game.js
+++ src/game/game.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
-import { MenuScene } from './scenes/MenuCleanTypographyScene.js';
+import { MenuScene } from './scenes/MenuTrackPresentationScene.js';
 import { RaceScene } from './scenes/RaceAntiCutPenaltyScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopUnifiedStyleScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
