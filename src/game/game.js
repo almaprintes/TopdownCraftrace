a9d04a4998f745625e38bb2f6c29588194e6cd3f
almaprintes <almaprint.es@gmail.com>
2026-08-15 14:49:20 +0100

    feat: enable reversible anti-cut penalty trial

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuCleanTypographyScene.js';
-import { RaceScene } from './scenes/RaceReplayDroneExportScene.js';
+import { RaceScene } from './scenes/RaceAntiCutPenaltyScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopCraftAssetsScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsAVOptionsScene.js';
