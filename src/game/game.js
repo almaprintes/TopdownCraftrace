0c26a30d2a289c80cb2309b67afa8cf44607f07f
almaprintes <almaprint.es@gmail.com>
2026-08-16 10:37:06 +0100

    Show only active survival rivals on the grid

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuCarPreviewFixScene.js';
-import { RaceScene } from './scenes/RaceSurvivalStartScene.js';
+import { RaceScene } from './scenes/RaceSurvivalVisibleRivalsScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopUnifiedStyleScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsAVOptionsScene.js';
