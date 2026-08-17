90439897d5f6451e3f942ca82247c869b874d338
almaprintes <almaprint.es@gmail.com>
2026-08-17 13:25:17 +0100

    Activate F1-style session sector report

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuTrackNameFitScene.js';
-import { RaceScene } from './scenes/RacePenaltyReportFixScene.js';
+import { RaceScene } from './scenes/RaceF1SessionReportScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopUnifiedStyleScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsHelpTutorialScene.js';
