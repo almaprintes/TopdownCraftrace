6422e69654aed83f41cd5bb82aa864d62df499bb
almaprintes <almaprint.es@gmail.com>
2026-08-18 19:03:56 +0100

    Activate clean real-track loading in Environment Builder

--- src/game/game.js
+++ src/game/game.js
@@ -11,7 +11,7 @@ import { CarEditorScene } from './scenes/CarEditorSpeedConsistencyScene.js';
 import { TrackEditorScene } from './scenes/TrackEditorScene.js';
 import { TrackGarageScene } from './scenes/TrackGarageCleanTypographyScene.js';
 import { TrackStudioScene } from './scenes/TrackStudioScene.js';
-import { EnvironmentBuilderScene } from './scenes/EnvironmentBuilderTrackVisiblePickerScrollScene.js';
+import { EnvironmentBuilderScene } from './scenes/EnvironmentBuilderRealTrackCleanScene.js';
 import { installMenuMusic } from './audio/MenuMusic.js';
 import './tracks/trackPublicNames.js';
 
