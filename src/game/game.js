0a75d6b364d2505fdb0cb21d5d213650653f0be8
almaprintes <almaprint.es@gmail.com>
2026-08-18 14:28:11 +0100

    Activate Environment Builder input priority fix

--- src/game/game.js
+++ src/game/game.js
@@ -11,7 +11,7 @@ import { CarEditorScene } from './scenes/CarEditorSpeedConsistencyScene.js';
 import { TrackEditorScene } from './scenes/TrackEditorScene.js';
 import { TrackGarageScene } from './scenes/TrackGarageCleanTypographyScene.js';
 import { TrackStudioScene } from './scenes/TrackStudioScene.js';
-import { EnvironmentBuilderScene } from './scenes/EnvironmentBuilderScrollScene.js';
+import { EnvironmentBuilderScene } from './scenes/EnvironmentBuilderInputFixScene.js';
 import { installMenuMusic } from './audio/MenuMusic.js';
 import './tracks/trackPublicNames.js';
 
