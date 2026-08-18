629b339f7cdfa5c69c97fb94aa04028943c9f8eb
almaprintes <almaprint.es@gmail.com>
2026-08-18 16:35:20 +0100

    Activate explicit real-track opening in Environment Builder

--- src/game/game.js
+++ src/game/game.js
@@ -11,7 +11,7 @@ import { CarEditorScene } from './scenes/CarEditorSpeedConsistencyScene.js';
 import { TrackEditorScene } from './scenes/TrackEditorScene.js';
 import { TrackGarageScene } from './scenes/TrackGarageCleanTypographyScene.js';
 import { TrackStudioScene } from './scenes/TrackStudioScene.js';
-import { EnvironmentBuilderScene } from './scenes/EnvironmentBuilderUILayoutScene.js';
+import { EnvironmentBuilderScene } from './scenes/EnvironmentBuilderOpenTrackScene.js';
 import { installMenuMusic } from './audio/MenuMusic.js';
 import './tracks/trackPublicNames.js';
 
