d1347a4fe4cc7ccbdbe4274682e744d5b2d47048
almaprintes <almaprint.es@gmail.com>
2026-08-19 10:44:33 +0100

    Activate pit-lap report label

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuAdminLogoRestoreScene.js';
-import { RaceScene } from './scenes/RaceKartingTenerifePaddockFinishScene.js';
+import { RaceScene } from './scenes/RaceKartingTenerifePitReportScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopUnifiedStyleScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsHelpTutorialScene.js';
