498301319f4ec0aaf9cac359701037e940e923ad
almaprintes <almaprint.es@gmail.com>
2026-08-22 20:49:54 +0100

    Wire Duel CPU1 guarantee scene

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuDuelModeScene.js';
-import { RaceScene } from './scenes/RaceDuelModeScene.js';
+import { RaceScene } from './scenes/RaceDuelCpuGuaranteeScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopCoinAssetScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsHelpTutorialScene.js';
