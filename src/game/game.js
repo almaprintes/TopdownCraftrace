50a25fadc657f82c8adea75fc31cffbfd42e04a0
almaprintes <almaprint.es@gmail.com>
2026-08-22 22:37:17 +0100

    Use Duel sector results scene

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuDuelModeScene.js';
-import { RaceScene } from './scenes/RaceDuelStandaloneScene.js';
+import { RaceScene } from './scenes/RaceDuelSectorResultsScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopCoinAssetScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsHelpTutorialScene.js';
