be95ac0dc63e08a1e40951bf53b9b4568b885f30
almaprintes <almaprint.es@gmail.com>
2026-08-22 18:54:38 +0100

    feat: wire official Duel mode scenes

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
-import { MenuScene } from './scenes/MenuDomUiScene.js';
-import { RaceScene } from './scenes/RaceKartingTenerifePitReportScene.js';
+import { MenuScene } from './scenes/MenuDuelModeScene.js';
+import { RaceScene } from './scenes/RaceDuelModeScene.js';
 import { UpgradeShopScene } from './scenes/UpgradeWorkshopCoinAssetScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsHelpTutorialScene.js';
