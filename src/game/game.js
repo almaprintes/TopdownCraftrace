195e6fc70802866325ffbcbcc846091776c28ad4
almaprintes <almaprint.es@gmail.com>
2026-08-23 13:13:39 +0100

    Enable homologation kit in workshop

--- src/game/game.js
+++ src/game/game.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser';
 import { BootScene } from './scenes/BootScene.js';
 import { MenuScene } from './scenes/MenuDuelModeScene.js';
 import { RaceScene } from './scenes/RaceDuelLapReplayScene.js';
-import { UpgradeShopScene } from './scenes/UpgradeWorkshopInventorySizingScene.js';
+import { UpgradeShopScene } from './scenes/UpgradeWorkshopHomologationKitScene.js';
 import { GarageScene } from './scenes/GarageCleanTypographyScene.js';
 import { SettingsScene } from './scenes/SettingsHelpTutorialScene.js';
 import { GarageDetailScene } from './scenes/GarageDetailSpeedConsistencyScene.js';
