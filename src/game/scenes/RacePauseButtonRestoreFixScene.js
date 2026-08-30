f9073fd2376cf571819379f98137269baef4022e
almaprintes <almaprint.es@gmail.com>
2026-08-30 11:22:44 +0100

    Remove obsolete race pause hotfix facade

--- src/game/scenes/RacePauseButtonRestoreFixScene.js
+++
@@ -1,3 +0,0 @@
-// Stable shipping entry point used by game.js.
-// Keep this facade tiny so future hotfixes do not create another inheritance maze.
-export { RaceScene } from './RaceShippingFinalV2Scene.js';
