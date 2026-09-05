fd7ad6cfe735b2a5b473dedb8096cd2743900948
almaprintes <almaprint.es@gmail.com>
2026-09-05 17:18:09 +0100

    Load RaceResult scene in shipping race flow

--- src/game/game.js
+++ src/game/game.js
@@ -25,7 +25,7 @@ const LAZY_SCENES={
   race:{
     load:async()=>{
       const [{RaceScene},{installExactRuntimeBeautyPass}]=await Promise.all([
-        import('./scenes/RaceExperienceScene.js'),
+        import('./scenes/RaceResultScene.js'),
         import('./scenes/raceExactRuntimeBeautyPass.js')
       ]);
       installExactRuntimeBeautyPass(RaceScene);
