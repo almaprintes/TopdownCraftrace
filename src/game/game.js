2a63bce33ddfb409c14cb82e861b76523cf25925
almaprintes <almaprint.es@gmail.com>
2026-09-12 08:31:09 +0100

    Wire fixed DOM ghost status panel

--- src/game/game.js
+++ src/game/game.js
@@ -25,7 +25,7 @@ const LAZY_SCENES={
   race:{
     load:async()=>{
       const [{RaceScene},{installExactRuntimeBeautyPass},{installRaceEnvironmentRuntime},{installRaceLoadingExperience},{installLiveDeltaReferenceRuntime}]=await Promise.all([
-        import('./scenes/RaceStaticMinimapScene.js'),
+        import('./scenes/RaceStaticGhostStatusScene.js'),
         import('./scenes/raceExactRuntimeBeautyPass.js'),
         import('./scenes/raceEnvironmentRuntime.js'),
         import('./ui/raceLoadingExperience.js'),
