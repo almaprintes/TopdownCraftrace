6bc5c24aec28c3854466813a30eeace923917f6b
almaprintes <almaprint.es@gmail.com>
2026-09-19 22:56:39 +0100

    DEV 1.1.22 remove obsolete subsystem instrumentation

--- src/game/scenes/RaceStaticGhostStatusScene.js
+++ src/game/scenes/RaceStaticGhostStatusScene.js
@@ -411,7 +411,6 @@ export class RaceScene extends CurrentRaceScene {
   }
 
   _syncStaticGhostPanel() {
-    const __t=performance.now();
     let found = null;
     if (!this._tdrGhostPanelAnchor || performance.now() < Number(this._tdrGhostPanelProbeUntil || 0)) {
       found = this._findGhostPanelObjects();
@@ -433,6 +432,5 @@ export class RaceScene extends CurrentRaceScene {
     const hiddenByReport = !!this._sessionReportOpen;
     const hiddenByReplay = !!this._replayActive;
     root.style.display = active && !hiddenByReport && !hiddenByReplay ? 'block' : 'none';
-    if(this._tdrCost)this._tdrCost.ghost=performance.now()-__t;
-  }
+    }
 }
